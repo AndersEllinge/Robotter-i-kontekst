ea4eea80b6c3a2c333e623ced3b270be44de12e6
Lordgreger <blacknedpsi@gmail.com>
2016-12-13 12:55:36 +0100

    Update of graph

--- Portfolio-3/Portfolio-3/Graph.h
+++ Portfolio-3/Portfolio-3/Graph.h
@@ -1,8 +1,39 @@
 #pragma once
+#include <vector>
+#include <stack>
+
+struct Coordinate
+{
+	int x;
+	int y;
+};
+
+struct AdjecencyObject
+{
+	Vertex* pointer;
+	int cost;
+};
+
+struct Vertex
+{
+	int cellKey;
+	Coordinate coordinate;
+	std::vector<AdjecencyObject> adjecencyList;
+};
+
 class Graph
 {
 public:
 	Graph();
 	~Graph();
+
+	void createGraph();
+	void addVertex();
+	std::stack<Coordinate> getPath();
+
+private:
+	std::vector<Vertex> vertices;
+
+
 };
 
